cd79854a36dfbb084a76b415ec3ea57cb632b5b4
Mario Barrera <31746007+MarioBarrera@users.noreply.github.com>
2020-06-05 18:49:04 -0500

    MB: Pantalla de inicio de sesión

---
+++ app/src/main/java/com/diplomado/coronaalert/Inicio_de_Sesion.java
@@ -0,0 +1,14 @@
+package com.diplomado.coronaalert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Inicio_de_Sesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_de__sesion);
+    }
+}
