a838a801f8da4b1429bfba1c288d057136251b9d
Diego Andrés Castañeda Hurtado <ffbifer@gmail.com>
2020-06-07 17:50:09 -0500

    DC: Texto en formulario de geolocalización

--- app/src/main/java/com/diplomado/coronaalert/InformeGeolocalizacionActivity.kt
+++ app/src/main/java/com/diplomado/coronaalert/InformeGeolocalizacionActivity.kt
@@ -3,7 +3,9 @@ package com.diplomado.coronaalert
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
@@ -11,21 +13,18 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 
 
 class InformeGeolocalizacionActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener{
 
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private lateinit var lastLocation: Location
+    private lateinit var mMap: GoogleMap
+
     companion object{
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1
     }
-
-    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,57 +37,36 @@ class InformeGeolocalizacionActivity : AppCompatActivity(), OnMapReadyCallback, 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
-        // Add a marker in Bogota and move the camera
-        //val bogota = LatLng(4.624335, -74.063644)
-        //mMap.addMarker(MarkerOptions().position(bogota).title("Ubícate en Bogotá."))
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(bogota))
-
-        //val cameraPosition = CameraPosition.builder().target(bogota).zoom(10f).build()
-
-        //mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-
         mMap.setOnMarkerClickListener(this)
         mMap.uiSettings.isZoomControlsEnabled = true
 
         setUpMap()
-
     }
 
     private fun placeMarket(location: LatLng){
         val markerOptions = MarkerOptions().position(location).title("Mi ubicación.")
-
         mMap.addMarker(markerOptions)
 
     }
 
     private fun setUpMap(){
         if(ActivityCompat.checkSelfPermission(this , android.Manifest.permission.ACCESS_FINE_LOCATION)
-         != PackageManager.PERMISSION_GRANTED){
+            != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this,
-            arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),LOCATION_PERMISSION_REQUEST_CODE)
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),LOCATION_PERMISSION_REQUEST_CODE)
             return
         }
         mMap.isMyLocationEnabled = true
         fusedLocationProviderClient.lastLocation.addOnSuccessListener(this) { location ->
 
-          if(location != null){
-              lastLocation = location
-              val currentLatLong = LatLng(location.latitude,location.longitude)
-              placeMarket(currentLatLong)
-              mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLong, 15f))
-          }
+            if(location != null){
+                lastLocation = location
+                val currentLatLong = LatLng(location.latitude,location.longitude)
+                placeMarket(currentLatLong)
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLong, 15f))
+            }
         }
     }
 
