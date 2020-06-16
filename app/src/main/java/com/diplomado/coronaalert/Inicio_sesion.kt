f165a0420b8b24da1418e04fe929f90a131d99bf
Diego Andrés Castañeda Hurtado <ffbifer@gmail.com>
2020-06-15 19:59:24 -0500

    DC: Se corrige bug inicio de sesión

--- app/src/main/java/com/diplomado/coronaalert/Inicio_sesion.kt
+++ app/src/main/java/com/diplomado/coronaalert/Inicio_sesion.kt
@@ -1,21 +1,22 @@
-                    package com.diplomado.coronaalert
+package com.diplomado.coronaalert
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_inicio_sesion.*
 
-                    class Inicio_sesion : AppCompatActivity() {
-
+class Inicio_sesion : AppCompatActivity() {
     private lateinit var txtUser: EditText
-    private lateinit var txtPassword:EditText
+    private lateinit var txtPassword: EditText
     private lateinit var progressBar: ProgressBar
-                        private lateinit var auth:FirebaseAuth
+    private lateinit var auth: FirebaseAuth
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,35 +30,38 @@ import com.google.firebase.auth.FirebaseAuth
 
 
     }
-                        fun forgotPassword(view:View){
 
-                        }
-                        fun register(view:View){
-                        startActivity(Intent(this,RegistroUsuario::class.java))
-                        }
-                        fun login(view:View){
-                            loginUser()
+    fun forgotPassword(view: View){
 
-                        }
-                        private fun loginUser(){
-                            val user:String=txtUser.toString()
-                            val password:String=txtPassword.toString()
-                            if(!TextUtils.isEmpty(user) && !TextUtils.isEmpty(password)){
-                                progressBar.visibility=View.VISIBLE
+    }
+    fun register(view:View){
+        startActivity(Intent(this,RegistroUsuario::class.java))
+    }
+    fun login(view:View){
+        loginUser()
 
-                                auth.signInWithEmailAndPassword(user,password)
-                                    .addOnCompleteListener(this){
-                                        task ->
+    }
+    private fun loginUser(){
 
-                                        if(task.isSuccessful){
-                                            action()
-                                        }else{
-                                            Toast.makeText(this,"Error en la autenticación", Toast.LENGTH_LONG).show()
-                                        }
-                                    }
-                            }
-                        }
-                        private fun action(){
-                            startActivity(Intent(this,MainActivity::class.java))
-                        }
+        val user:String= editEmail.text.toString().trim()
+        val password:String= editPassword.text.toString().trim()
+        if(!TextUtils.isEmpty(user) && !TextUtils.isEmpty(password)){
+            progressBar.visibility=View.VISIBLE
+            Log.e("usuario",user.toString())
+            auth.signInWithEmailAndPassword(user.trim(),password)
+                .addOnCompleteListener(this){
+                        task ->
+
+                    if(task.isSuccessful){
+                        action()
+                    }else{
+                        progressBar.visibility=View.INVISIBLE
+                        Toast.makeText(this,task.exception?.message.toString(), Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
+    }
+    private fun action(){
+        startActivity(Intent(this,MainActivity::class.java))
+    }
 }
